f255f255ffbf4112bf02ec36c314e6501e595e1d
rafaelsilva <todosomtodavoz@gmail.com>
2025-05-14 08:49:14 -0300

    start project

---
+++ cypress/e2e/open_site.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://demoqa.com/')
+
+    cy.get('header img[src="/images/Toolsqa.jpg"]', { timeout: 10000 })
+    .should('be.visible')
+  })
+})
