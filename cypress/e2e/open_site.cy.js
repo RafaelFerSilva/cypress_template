178107f4839bfdfb119391ed4a7f0122ee353229
rafaelsilva <todosomtodavoz@gmail.com>
2025-05-17 18:30:23 -0300

    chore: config steps

--- cypress/e2e/open_site.cy.js
+++ cypress/e2e/open_site.cy.js
@@ -1,6 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
+
+describe('Home Page Test Suite', () => {
+  it('Home page test', () => {
+    cy.allure().step('Visit Home Page')
     cy.visit('https://demoqa.com/')
+    
+    cy.allure().step('Validate home page is loaded successfully')
     cy.get('header img[src="/images/Toolsqa.jpg"]', { timeout: 10000 })
     .should('be.visible')
   })
