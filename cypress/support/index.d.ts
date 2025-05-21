83f680fe8646c8ce7c0f62086ba17471b1e3d480
rafaelsilva <todosomtodavoz@gmail.com>
2025-05-21 09:30:20 -0300

    feat: return user

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,7 +2,8 @@ declare namespace Cypress {
   interface Chainable<Subject = any> {
     dbConnect(): Chainable<void>;
     dbClose(): Chainable<void>;
-    dbExecuteScript(scriptPath: string): Chainable<void>;
-    dbReplaceAndExecute(scriptPath: string, values: any[]): Chainable<void>;
+    dbExecuteScript(scriptPath: string): Chainable<any[]>;
+    dbReplaceAndExecute(scriptPath: string, values: any[]): Chainable<any[]>;
+    returnAllUsers(): Chainable<any[]>;
   }
 }
